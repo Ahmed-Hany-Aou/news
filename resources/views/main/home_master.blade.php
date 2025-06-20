34b31733f418aa4432d4549ae77fa8bed79b7823
Ahmed Hany <ahmed.hany.boshra@gmail.com>
2025-06-20 23:16:16 +0300

    implemented social links in frontend

--- resources/views/main/home_master.blade.php
+++ resources/views/main/home_master.blade.php
@@ -1,9 +1,17 @@
+@php
+$seo=DB::table('seos')->first();
+@endphp
 <!DOCTYPE html>
 <html lang="en">
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="author" content="{{ $seo->meta_author}}">
+        <meta name="keyword" content="{{ $seo->meta_keyword}}">
+        <meta name="description" content="{{ $seo->meta_description}}">
+        <meta name="google_verification" content="{{ $seo->google_verification}}">
+
         <title>Easy Online News Site</title>
          <link href=" {{ asset('frontend/assets/css/bootstrap.min.css') }}" rel="stylesheet">
         <link href=" {{ asset('frontend/assets/css/menu.css') }}" rel="stylesheet">
